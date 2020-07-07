ef33f301ae3f99f19dc24bfe2b7fe26bc8e1e4d5
abdouzxdz <67341857+abdouzxdz@users.noreply.github.com>
2020-07-07 22:21:25 +0200

    Create ipscaner.py

---
+++ ipscaner.py
@@ -0,0 +1,18 @@
+d_user = "admin"
+d_passw = "passw"
+user =input("enrer user")
+passw =input ("enter pass")
+if user==d_user and passw== d_passw:
+    print ("chose 1 to scan ip")
+    print ("chose 2 to to exit")
+    x = input ("enter number")
+    if x == ("1"):
+        ip = input("enter ip")
+        import test_ipaddress
+        print ("not found")
+    if x == ("2"):
+        print ("thank you bey")
+    else:
+        print ("chose teru number")
+        import xz
+                
